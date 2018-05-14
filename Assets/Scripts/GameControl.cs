4cee4ba8e468922ea316e9c80caa15f3f64cf0d7
Kevin Theiss <ktheiss1990@gmail.com>
2018-05-14 14:36:53 -0700

    Added global GameControl script and score text.

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour {
+
+    public static GameControl control;
+
+    public float lives;
+    public float score;
+    public float level;
+
+    // Use this for initialization
+    void Awake()
+    {
+        if (control == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            control = this;
+        }
+        else if (control != this)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnGUI()
+    {
+        GUIStyle style = new GUIStyle();
+        style.fontSize = 28;
+        style.normal.textColor = Color.white;
+        GUI.Label(new Rect(new Vector2(10f, 10f), new Vector2(80f, 50f)), "Score: " + score, style);
+    }
+}
---
+++ Assets/Scripts/TransitionScene.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TransitionScene : MonoBehaviour {
+
+    public int sceneToLoad;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+	}
+}
